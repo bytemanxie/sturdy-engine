67f0ef58999adb190cebfcb862557cce4ff68754
xzj <783638806@qq.com>
2023-07-24 23:52:20 +0800

    项目代码的建立

---
+++ RemoteCtrl/RemoteClient/RemoteClient.h
@@ -0,0 +1,32 @@
+﻿
+// RemoteClient.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CRemoteClientApp:
+// See RemoteClient.cpp for the implementation of this class
+//
+
+class CRemoteClientApp : public CWinApp
+{
+public:
+	CRemoteClientApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CRemoteClientApp theApp;
