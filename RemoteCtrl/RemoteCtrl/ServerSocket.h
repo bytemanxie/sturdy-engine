dee370fe6c2121de15ce0bf666b7fe4125219540
xzj <783638806@qq.com>
2023-07-27 21:28:51 +0800

    数据包

--- RemoteCtrl/RemoteCtrl/ServerSocket.h
+++ RemoteCtrl/RemoteCtrl/ServerSocket.h
@@ -1,6 +1,90 @@
 #pragma once
 #include "pch.h"
 #include "framework.h"
+class CPacket
+{
+public:
+	CPacket() :sHead(0), nLength(0), sCmd(0), sSum(0) {}
+	CPacket(WORD nCmd, const BYTE* pData, size_t nSize) {
+		sHead = 0xFEFF;
+		nLength = nSize + 4;
+		sCmd = nCmd;
+		if (nSize > 0) {
+			strData.resize(nSize);
+			memcpy((void*)strData.c_str(), pData, nSize);
+		}
+		else {
+			strData.clear();
+		}
+		sSum = 0;
+		for (size_t j = 0; j < strData.size(); j++)
+		{
+			sSum += BYTE(strData[j]) & 0xFF;
+		}
+	}
+	CPacket(const CPacket& pack) {//拷贝构造函数
+		sHead = pack.sHead;
+		nLength = pack.nLength;
+		sCmd = pack.sCmd;
+		strData = pack.strData;
+		sSum = pack.sSum;
+	}
+	CPacket(const BYTE* pData, size_t& nSize) {
+		size_t i = 0;
+		for (; i < nSize; i++) {//找包头
+			if (*(WORD*)(pData + i) == 0xFEFF) {
+				sHead = *(WORD*)(pData + i);
+				i += 2;
+				break;
+			}
+		}
+		if (i + 4 + 2 + 2 > nSize) {//包数据可能不全，或者包头未能全部接收到
+			nSize = 0;
+			return;
+		}
+		nLength = *(DWORD*)(pData + i); i += 4;
+		if (nLength + i > nSize) {//包未完全接收到，就返回，解析失败
+			nSize = 0;
+			return;
+		}
+		sCmd = *(WORD*)(pData + i); i += 2;
+		if (nLength > 4) {
+			strData.resize(nLength - 2 - 2);
+			memcpy((void*)strData.c_str(), pData + i, nLength - 4);
+			i += nLength - 4;
+		}
+		sSum = *(WORD*)(pData + i); i += 2;
+		WORD sum = 0;
+		for (size_t j = 0; j < strData.size(); j++)
+		{
+			sum += BYTE(strData[j]) & 0xFF;
+		}
+		if (sum == sSum) {
+			nSize = i;//head2 length4 data...
+			return;
+		}
+		nSize = 0;
+	}
+	~CPacket() {}
+	CPacket& operator=(const CPacket& pack) {
+		if (this != &pack) {
+			sHead = pack.sHead;
+			nLength = pack.nLength;
+			sCmd = pack.sCmd;
+			strData = pack.strData;
+			sSum = pack.sSum;
+		}
+		return *this;
+	}
+
+public:
+	WORD sHead;//固定位 0xFEFF
+	DWORD nLength;//包长度（从控制命令开始，到和校验结束）
+	WORD sCmd;//控制命令
+	std::string strData;//包数据
+	WORD sSum;//和校验
+};
+
 class CServerSocket
 {
 public:
@@ -37,16 +121,37 @@ public:
 		if (m_client == -1) return false;
 		return true;
 	}
-	int DealCommand()
-	{
-		if (m_client == -1) return false;
-		char buffer[1024] = "";
-		while (true)
-		{
-			int len = recv(m_client, buffer, sizeof buffer, 0);
-			if (len <= 0)return -1;
-			//TODO:处理命令
+
+#define BUFFER_SIZE 4096
+	int DealCommand() {//处理命令
+		if (m_client == -1)return -1;
+		//char buffer[1024] = "";
+		char* buffer = new char[BUFFER_SIZE];
+		if (buffer == NULL) {
+			TRACE("内存不足！\r\n");
+			return -2;
 		}
+		memset(buffer, 0, BUFFER_SIZE);
+		size_t index = 0;
+		while (true) {
+			size_t len = recv(m_client, buffer + index, BUFFER_SIZE - index, 0);
+			if (len <= 0) {
+				delete[]buffer;
+				return -1;
+			}
+			TRACE("recv %d\r\n", len);
+			index += len;
+			len = index;
+			m_packet = CPacket((BYTE*)buffer, len);
+			if (len > 0) {
+				memmove(buffer, buffer + len, BUFFER_SIZE - len);
+				index -= len;
+				delete[]buffer;
+				return m_packet.sCmd;
+			}
+		}
+		delete[]buffer;
+		return -1;
 	}
 	bool Send(const char* pData, int nSize)
 	{
@@ -56,6 +161,7 @@ public:
 
 private:
 	SOCKET m_sock, m_client;
+	CPacket m_packet;
 	CServerSocket& operator=(const CServerSocket& ss)
 	{
 
