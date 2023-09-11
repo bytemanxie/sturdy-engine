ff37988d4d498ec1bc57b268528ce6503e56773b
xzj <783638806@qq.com>
2023-09-11 10:14:08 +0800

    1 代码重构 2 代码优化

--- RemoteCtrl/RemoteCtrl/EdoyunTool.h
+++ RemoteCtrl/RemoteCtrl/EdoyunTool.h
@@ -15,5 +15,153 @@ public:
 		strOut += "\n";
 		OutputDebugStringA(strOut.c_str());
 	}
+
+	static bool isAdmin()
+	{
+		HANDLE hToken = NULL;
+		if (!OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken))
+		{
+			ShowError();
+			return false;
+		}
+
+		TOKEN_ELEVATION eve;
+		DWORD len = 0;
+		if (GetTokenInformation(hToken, TokenElevation, &eve, sizeof eve, &len) == FALSE)
+		{
+			ShowError();
+			return false;
+		}
+
+		CloseHandle(hToken);
+		if (len == sizeof eve)
+		{
+			return eve.TokenIsElevated;
+		}
+
+		printf("length of token information is %d\r\n", len);
+		return false;
+	}
+
+	static bool RunAsAdmin()
+	{
+		//获取管理员权限， 使用该权限创建线程
+		//本地策略组 开启administrator账户，禁止空密码只能登录本地控制台
+		STARTUPINFO si = { 0 };
+		PROCESS_INFORMATION pi = { 0 };
+		TCHAR sPath[MAX_PATH] = _T("");
+		//GetCurrentDirectory(MAX_PATH, sPath);
+		GetModuleFileName(NULL, sPath, MAX_PATH);
+
+		CString strCmd = sPath;
+		strCmd += _T("\\RemoteCtrl.exe");
+
+		/*ret = CreateProcessWithTokenW(hToken, LOGON_WITH_PROFILE, NULL,
+			(LPWSTR)(LPCWSTR)strCmd, CREATE_UNICODE_ENVIRONMENT, NULL, NULL, &si, &pi);*/
+		BOOL ret = CreateProcessWithLogonW(_T("Administrator"), NULL, NULL,
+			LOGON_WITH_PROFILE, NULL,
+			(LPWSTR)(LPCWSTR)strCmd, CREATE_UNICODE_ENVIRONMENT,
+			NULL, NULL, &si, &pi);
+
+		if (!ret)
+		{
+			ShowError();
+			MessageBox(NULL, _T("创建进程失败！"), _T("程序错误！"), 0);
+			return false;
+		}
+
+		WaitForSingleObject(pi.hProcess, INFINITE);
+		CloseHandle(pi.hProcess);
+		CloseHandle(pi.hThread);
+	}
+
+	static void ShowError()
+	{
+		LPWSTR lpMessageBuf = NULL;
+		FormatMessage(
+			FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER,
+			NULL, GetLastError(),
+			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+			lpMessageBuf, 0, NULL
+		);
+		OutputDebugString(lpMessageBuf);
+		LocalFree(lpMessageBuf);
+		::exit(0);
+	}
+
+	static BOOL WriteStartupDir(const CString& strPath)
+	{//通过修改开机启动文件夹来实现开机启动
+		TCHAR sPath[MAX_PATH] = _T("");
+		GetModuleFileName(NULL, sPath, MAX_PATH);
+
+		return CopyFile(sPath, strPath, FALSE);
+	}
+
+	static bool WriteRegisterTable(const CString& strPath)
+	{//通过修改注册表来实现开机启动
+		CString strSubkey = _T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
+		TCHAR sPath[MAX_PATH] = _T("");
+		char sSys[MAX_PATH] = "";
+		std::string strExe = "\\RemoteCtrl.exe ";
+
+		GetModuleFileName(NULL, sPath, MAX_PATH);
+
+		BOOL ret = CopyFile(sPath, strPath, FALSE);
+		if (ret == FALSE)
+		{
+			MessageBox(NULL, _T("复制文件失败，是否权限不足？\r\n"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
+			return false;
+		}
+
+		HKEY hKey = NULL;
+		ret = RegOpenKeyEx(HKEY_LOCAL_MACHINE, strSubkey, 0, KEY_WRITE | KEY_WOW64_64KEY, &hKey);
+		if (ret != ERROR_SUCCESS)
+		{
+			RegCloseKey(hKey);
+			MessageBox(NULL, _T("设置自动开机启动失败！是否权限不足？\r\n程序启动失败！"),
+				_T("错误"),
+				MB_ICONERROR | MB_TOPMOST
+			);
+			return false;
+		}
+		ret = RegSetValueEx(
+			hKey,
+			_T("RemoteCtrl"),
+			0,
+			REG_EXPAND_SZ,
+			(BYTE*)(LPCTSTR)strPath,
+			strPath.GetLength() * sizeof(TCHAR)
+		);
+
+		if (ret != ERROR_SUCCESS)
+		{
+			RegCloseKey(hKey);
+			MessageBox(NULL, _T("设置自动开机启动失败！是否权限不足？\r\n程序启动失败！"),
+				_T("错误"),
+				MB_ICONERROR | MB_TOPMOST
+			);
+			return false;
+		}
+		RegCloseKey(hKey);
+		return true;
+	}
+
+	static bool Init()
+	{//所有MFC应用的初始化
+		HMODULE hModule = ::GetModuleHandle(nullptr);
+		if (hModule == nullptr)
+		{
+			wprintf(L"Fatal Error: GetModuleHandle failed\n");
+			return false;
+		}
+
+		if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
+		{
+			// TODO: code your application's behavior here.
+			wprintf(L"Fatal Error: MFC initialization failed\n");
+			return false;
+		}
+	}
+
 };
 
--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -12,85 +12,22 @@
 #endif
 #include "EdoyunTool.h"
 
+#define INVOKE_PATH _T("C:\\Users\\edoyun\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\RemoteCtrl.exe")
 
 // The one and only application object
 
 CWinApp theApp;
 
-void WriteRegisterTable(const CString& strPath)
+
+
+
+
+bool ChooseAutoInvoke(const CString& strPath)
 {
-	CString strSubkey = _T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
-	char sPath[MAX_PATH] = "";
-	char sSys[MAX_PATH] = "";
-	std::string strExe = "\\RemoteCtrl.exe ";
-	GetCurrentDirectoryA(MAX_PATH, sPath);
-
-	GetSystemDirectoryA(sSys, sizeof sSys);
-	std::cout << sPath << ' ' << sSys << std::endl;
-	std::string strCmd = "mklink " + std::string(sSys)
-		+ strExe + std::string(sPath) + strExe;
-	std::cout << strCmd << std::endl;
-	int ret = system(strCmd.c_str());
-
-	HKEY hKey = NULL;
-	ret = RegOpenKeyEx(HKEY_LOCAL_MACHINE, strSubkey, 0, KEY_WRITE | KEY_WOW64_64KEY, &hKey);
-	if (ret != ERROR_SUCCESS)
-	{
-		RegCloseKey(hKey);
-		MessageBox(NULL, _T("设置自动开机启动失败！是否权限不足？\r\n程序启动失败！"),
-			_T("错误"),
-			MB_ICONERROR | MB_TOPMOST
-		);
-		exit(0);
-	}
-	ret = RegSetValueEx(
-		hKey,
-		_T("RemoteCtrl"),
-		0,
-		REG_EXPAND_SZ,
-		(BYTE*)(LPCTSTR)strPath,
-		strPath.GetLength() * sizeof(TCHAR)
-	);
-
-	if (ret != ERROR_SUCCESS)
-	{
-		RegCloseKey(hKey);
-		MessageBox(NULL, _T("设置自动开机启动失败！是否权限不足？\r\n程序启动失败！"),
-			_T("错误"),
-			MB_ICONERROR | MB_TOPMOST
-		);
-		exit(0);
-	}
-	RegCloseKey(hKey);
-}
-
-void WriteStartupDir(const CString& strPath)
-{
-	CString strCmd = GetCommandLine();
-	strCmd.Replace(_T("\""), _T(""));
-	BOOL ret = CopyFile(strCmd, strPath, FALSE);
-	if (ret == FALSE)
-	{
-		MessageBox(NULL, _T("复制文件失败，是否权限不足？\r\n"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
-		::exit(0);
-	}
-
-}
-
-void ChooseAutoInvoke()
-{
-	TCHAR wcsSystem[MAX_PATH] = _T("");
-	//GetSystemDirectory(wcsSystem, MAX_PATH);
-	
-	//CString strPath = CString(_T("C:\\Windows\\SysWOW64\\RemoteCtrl.exe"));
-	CString strPath = _T("C:\\Users\\edoyun\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\RemoteCtrl.exe");
-	std::cout << 1 << strPath.GetString() << std::endl;
 	if (PathFileExists(strPath))
 	{
-		std::cout << 2 << strPath << std::endl;
-		return; 
+		return true; 
 	}
-
 	
 	CString strInfo = _T("该程序只允许用于合法的用途！\n");
 	strInfo += _T("继续运行该程序，将使得这台机器处于被监控的状态！\n");
@@ -102,151 +39,63 @@ void ChooseAutoInvoke()
 	if (ret == IDYES)
 	{
 		//WriteRegisterTable(strPath);
-		WriteStartupDir(strPath);
+		if (!CEdoyunTool::WriteStartupDir(strPath))
+		{
+			MessageBox(NULL, _T("复制文件失败，是否权限不足？\r\n"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
+			return false;
+		}
+
 	}
 	else if (ret == IDCANCEL)
 	{
-		exit(0);
-	}
-	return;
-}
-
-void ShowError()
-{
-	LPWSTR lpMessageBuf = NULL;
-	FormatMessage(
-		FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER,
-		NULL, GetLastError(),
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		lpMessageBuf, 0, NULL
-	);
-	OutputDebugString(lpMessageBuf);
-	LocalFree(lpMessageBuf);
-	::exit(0);
-}
-
-bool isAdmin()
-{
-	HANDLE hToken = NULL;
-	if (!OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken))
-	{
-		ShowError();
 		return false;
 	}
-	TOKEN_ELEVATION eve;
-	DWORD len = 0;
-	if (GetTokenInformation(hToken, TokenElevation, &eve, sizeof eve, &len) == FALSE)
-	{
-		ShowError();
-		return false;
-	}
-
-	CloseHandle(hToken);
-	if (len == sizeof eve)
-	{
-		return eve.TokenIsElevated;
-	}
-
-	printf("length of token information is %d\r\n", len);
-	return false;
+	return true;
 }
 
-void RunAsAdmin()
-{
-	HANDLE hToken = NULL;
-	BOOL ret = LogonUser(L"Administrator", NULL, NULL, LOGON32_LOGON_BATCH,
-		LOGON32_PROVIDER_DEFAULT, &hToken);
-	if (!ret)
-	{
-		ShowError();
-		MessageBox(NULL, _T("登陆错误！"), _T("程序错误"), 0);
-		::exit(0);
-	}
-	OutputDebugString(L"Logon administrator success!\r\n");
-	STARTUPINFO si = { 0 };
-	PROCESS_INFORMATION pi = { 0 };
-	TCHAR sPath[MAX_PATH] = _T("");
-	GetCurrentDirectory(MAX_PATH, sPath);
-	CString strCmd = sPath;
-	strCmd += _T("\\RemoteCtrl.exe");
 
-	/*ret = CreateProcessWithTokenW(hToken, LOGON_WITH_PROFILE, NULL, 
-		(LPWSTR)(LPCWSTR)strCmd, CREATE_UNICODE_ENVIRONMENT, NULL, NULL, &si, &pi);*/
-	ret = CreateProcessWithLogonW(_T("Administrator"), NULL, NULL, LOGON_WITH_PROFILE,
-		NULL, (LPWSTR)(LPCWSTR)strCmd, CREATE_UNICODE_ENVIRONMENT, NULL, NULL, &si, &pi);
-	CloseHandle(hToken);
-
-	if (!ret)
-	{
-		ShowError();
-		MessageBox(NULL, _T("创建进程失败！"), _T("程序错误！"), 0);
-		::exit(0);
-	}
-
-	WaitForSingleObject(pi.hProcess, INFINITE);
-	CloseHandle(pi.hProcess);
-	CloseHandle(pi.hThread);
-}
 
 int main()
 {
-	int nRetCode = 0;
-	HMODULE hModule = ::GetModuleHandle(nullptr);
-
-	if (hModule != nullptr)
+	if (CEdoyunTool::isAdmin())
 	{
-		// initialize MFC and print and error on failure
-		if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
+		if (!CEdoyunTool::Init()) return 1;
+		printf("current is run as administrator!\r\n");
+		MessageBox(NULL, _T("管理员"), _T("用户状态"), 0);
+
+		//TODO: code your application's behavior here.
+		CCommand cmd;
+		if (!ChooseAutoInvoke(INVOKE_PATH))
 		{
-			// TODO: code your application's behavior here.
-			wprintf(L"Fatal Error: MFC initialization failed\n");
-			nRetCode = 1;
+			::exit(0);
 		}
-		else
+		int ret = CServerSocket::getInstance()->Run(&CCommand::RunCommand, &cmd);
+
+		switch (ret)
 		{
-			if (isAdmin())
-			{
-				printf("current is run as administrator!\r\n");
-				MessageBox(NULL, _T("管理员"), _T("用户状态"), 0);
-
-			}
-			else
-			{
-				printf("current is run as normal user!\r\n");
-				
-				RunAsAdmin();
-				MessageBox(NULL, _T("管理员"), _T("用户状态"), 0);
-				return nRetCode;
-			}
-			//TODO: code your application's behavior here.
-			CCommand cmd;
-			ChooseAutoInvoke();
-			int ret = CServerSocket::getInstance()->Run(&CCommand::RunCommand, &cmd);
-
-			switch (ret)
-			{
-			case -1:
-				MessageBox(NULL, _T("网络初始化异常！"),
-					_T("网络初始化失败！"),
-					MB_OK | MB_ICONERROR);
-				exit(0);
-				break;
-			case 2:
-				MessageBox(NULL, _T("多次无法正常接入用户，结束程序！"), 
-					_T("接入用户失败！"),
-					MB_OK | MB_ICONERROR);
-				exit(0);
-			default:
-				break;
-			}
+		case -1:
+			MessageBox(NULL, _T("网络初始化异常！"),
+				_T("网络初始化失败！"),
+				MB_OK | MB_ICONERROR);
+			break;
+		case 2:
+			MessageBox(NULL, _T("多次无法正常接入用户，结束程序！"),
+				_T("接入用户失败！"),
+				MB_OK | MB_ICONERROR);
+		default:
+			break;
 		}
 	}
 	else
 	{
-		// TODO: change error code to suit your needs
-		wprintf(L"Fatal Error: GetModuleHandle failed\n");
-		nRetCode = 1;
+		printf("current is run as normal user!\r\n");
+
+		if (CEdoyunTool::RunAsAdmin() == false)
+		{
+			CEdoyunTool::ShowError();
+		}
+		return 0;
 	}
 
-	return nRetCode;
+	return 0;
 }
